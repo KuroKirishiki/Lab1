00e738ada76fe663408314457816f150e4ceb285
student <nskden2004nsk23@gmail.com>
2023-02-10 16:23:18 +0300

    Lab1

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'yellow')
+        square(body.x, body.y, 9, 'black')
 
     square(food.x, food.y, 9, 'green')
     update()
